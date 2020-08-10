b7a6314e48e4dd75a9a78591786835556df6782a
SK-aped <som.kherwa@momentoustech.com>
2020-08-10 14:31:46 +0530

    base commit

---
+++ jwtVanilla/src/main/java/com/somhome/JwtVanillaApplication.java
@@ -0,0 +1,13 @@
+package com.somhome;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JwtVanillaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JwtVanillaApplication.class, args);
+	}
+
+}
---
+++ jwtVanilla/src/test/java/com/somhome/JwtVanillaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.somhome;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JwtVanillaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
